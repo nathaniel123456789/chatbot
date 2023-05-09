f3c5b3f5321bdfa8840acf69a84197cebabc20cb
Nathaniel Pyon <nathaniel.pyon@mrjaffesclass.com>
2023-05-09 11:32:59 -0700

    new code

--- activity4/Magpie4.java
+++ activity4/Magpie4.java
@@ -99,7 +99,21 @@ public class Magpie4
         return "What would it mean to " + restOfStatement + "?";
     }
 
-    
+    private String transformIWantStatement(String statement)
+    {
+        //  Remove the final period, if there is one
+        statement = statement.trim();
+        String lastChar = statement.substring(statement
+                .length() - 1);
+        if (lastChar.equals("."))
+        {
+            statement = statement.substring(0, statement
+                    .length() - 1);
+        }
+        int psn = findKeyword (statement, "I want", 0);
+        String restOfStatement = statement.substring(psn + 6).trim();
+        return "Why do you want " + restOfStatement + "?";
+    }
     
     /**
      * Take a statement with "you <something> me" and transform it into 
